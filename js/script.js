dd59bfa7db5e977eb35592ab5a0683da77a307a0
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-17 19:34:26 +0200

    Create getData and checkCategory functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var boxArr = document.querySelectorAll('.container__box');
+var headerArr = document.querySelectorAll('.container__header');
+var contentArr = document.querySelectorAll('p');
 var data = [
 	{
 		id: 'box1',
@@ -33,25 +36,26 @@ var data = [
 	},
 ];
 
-var boxArr = document.querySelectorAll('.container__box');
-var headerArr = document.querySelectorAll('.container__header');
-var contentArr = document.querySelectorAll('p');
+var getData = function(scr, box, header, content, iterator) {
+	box[iterator].setAttribute('id',scr[iterator].id);
+	header[iterator].innerHTML = scr[iterator].title;
+	content[iterator].innerHTML = scr[iterator].content;
+}
+
+var checkCategory = function(array, iterator, className) {
+	for (var key in array) {
+		if (array[key].length > 0 && array[key] !== className) {
+			boxArr[iterator].classList.add(array[key]);
+		} else if (array[key] === className) {
+			headerArr[iterator].classList.add(array[key]);
+		}
+	}	
+};
 
 for (var i = 0; i < boxArr.length; i++) {
-	boxArr[i].setAttribute('id',data[i].id);
-  headerArr[i].innerHTML = data[i].title;
-  contentArr[i].innerHTML = data[i].content;
-	
-	(function() {
-		var categoryArr = data[i].categories;
-		for (var j = 0; j < categoryArr.length; j++) {
-			if (categoryArr[j].length > 0 && categoryArr[j] != 'special-header') {
-				boxArr[i].classList.add(categoryArr[j]);
-			} else if (categoryArr[j] == 'special-header') {
-				headerArr[i].classList.add(categoryArr[j]);
-			}
-		};
-	})();
+	var categoryArr = data[i].categories;
+	getData(data, boxArr, headerArr, contentArr, i);
+	checkCategory(categoryArr, i, 'special-header');
 };
 
 
